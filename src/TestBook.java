93f0e3b951937638cef6e52aa1e464c268cddfa6
Victor Alonso <victor@ieselcaminas.org>
2019-10-28 15:55:44 +0100

    xxx

--- src/TestBook.java
+++ src/TestBook.java
@@ -6,6 +6,11 @@ public class TestBook {
 		Author a = new Author("Richard Rothfuss", "richardr@gmail.com",
 				'm');
 		Book b = new Book("The name of the wind", a, 30.9, 10);
+		Book b2 = new Book("The notebook",
+					new Author("Patrick Smith", "ppp@gmail.com", 'm'),
+					25.9);
+		Book b3 = new Book("The fear of a wise man", a, 35.9, 8);
+		Book b4 = b;
 		
 
 	}
