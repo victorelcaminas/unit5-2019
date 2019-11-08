786934c6e7a629d6b133927b44249b660e9c8f6b
Victor Alonso <victor@ieselcaminas.org>
2019-11-08 16:50:53 +0100

    Multiply

--- src/MyPolynomial.java
+++ src/MyPolynomial.java
@@ -62,10 +62,49 @@ public class MyPolynomial {
 	
 	
 	public MyPolynomial add(MyPolynomial p) {
+		double[] result, biggest, smallest;
+		if (coeffs.length > p.coeffs.length) {
+			biggest = coeffs;
+			smallest = p.coeffs;
+		} else {
+			biggest = p.coeffs;
+			smallest = coeffs;
+		}
+		result = new double[biggest.length];
+		for (int i = 0; i < biggest.length; i++) {
+			result[i] = biggest[i];
+		}
+		for (int i = 0; i < smallest.length; i++) {
+			result[i] += smallest[i];
+		}
+		return new MyPolynomial(result);
 		
 	}
 	
+	public MyPolynomial multiply(MyPolynomial p) {
+		double[] result = 
+				new double[coeffs.length + p.coeffs.length - 1];
+		
+		for (int i = 0; i < coeffs.length; i ++) {
+			for (int j = 0; j < p.coeffs.length; j ++) {
+				result[i + j] = coeffs[i] * p.coeffs[j];
+			}
+		}
+		
+		return new MyPolynomial(result);
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/MyPolynomialMain.java
+++ src/MyPolynomialMain.java
@@ -4,15 +4,13 @@ public class MyPolynomialMain {
 	public static void main(String[] args) {
 		
 		MyPolynomial p1 = new MyPolynomial(1.1, 2.2, 3.3);  
-		MyPolynomial p2 = new MyPolynomial(1.1, 2.2, 3.3, 4.4, 5.5); 
+		MyPolynomial p2 = new MyPolynomial(1.1, 2.2, 0.0, 4.4, 5.5); 
 		// Can also invoke with an array  
-		double[] coeffs = {3.0, 6.0, 2.0, 3.0, 0.0, 5.0}; 
-		MyPolynomial p3 = new MyPolynomial(coeffs);
-		MyPolynomial p4 = new MyPolynomial("poly");
 		
-		System.out.println(p3);
+		System.out.println(p1);
+		System.out.println(p2);
 		
-		MyPolynomial p5 = p3.add(p2);
+		System.out.println(p1.add(p2));
 
 	}
 
